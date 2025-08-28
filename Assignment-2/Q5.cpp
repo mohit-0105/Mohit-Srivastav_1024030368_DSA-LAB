032cd7b0ce567010eabf08c2a5ac9ffc31c68c5c
Mohit Srivastav <mohit.srivastav2006@gmail.com>
2025-08-28 23:51:30 +0530

    Update Q5.cpp

--- Assignment-2/Q5.cpp
+++ Assignment-2/Q5.cpp
@@ -1,39 +1,77 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 class DiagonalMatrix {
-    int *arr;
-    int n;
-
+    int *A, n;
 public:
-    DiagonalMatrix(int size) {
-        n = size;
-        arr = new int[n]();
+    DiagonalMatrix(int n) {
+        this->n = n;
+        A = new int[n];
     }
-
-    void set(int i, int j, int val) {
-        if (i == j)
-            arr[i] = val;
+    void set(int i,int j,int x) {
+        if(i==j) A[i-1] = x;
     }
-
-    int get(int i, int j) {
-        if (i == j)
-            return arr[i];
-        return 0;
+    int get(int i,int j) {
+        if(i==j) return A[i-1];
+        else return 0;
     }
-
-    ~DiagonalMatrix() {
-        delete[] arr;
+    void display() {
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=n;j++) {
+                if(i==j) cout<<A[i-1]<<" ";
+                else cout<<"0 ";
+            }
+            cout<<endl;
+        }
     }
 };
 
-int main() {
-    DiagonalMatrix d(4);
-    d.set(0, 0, 5);
-    d.set(1, 1, 10);
+class LowerTriangular {
+    int *A, n;
+public:
+    LowerTriangular(int n) {
+        this->n=n;
+        A=new int[n*(n+1)/2];
+    }
+    void set(int i,int j,int x) {
+        if(i>=j) A[i*(i-1)/2 + j-1] = x;
+    }
+    int get(int i,int j) {
+        if(i>=j) return A[i*(i-1)/2 + j-1];
+        else return 0;
+    }
+    void display() {
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=n;j++) {
+                if(i>=j) cout<<A[i*(i-1)/2+j-1]<<" ";
+                else cout<<"0 ";
+            }
+            cout<<endl;
+        }
+    }
+};
 
-    cout << "Element at (1,1): " << d.get(1, 1) << endl;
-    cout << "Element at (1,2): " << d.get(1, 2) << endl;  
-    return 0;
-}
-
+class UpperTriangular {
+    int *A, n;
+public:
+    UpperTriangular(int n) {
+        this->n=n;
+        A=new int[n*(n+1)/2];
+    }
+    void set(int i,int j,int x) {
+        if(i<=j) A[n*(i-1) - (i-2)*(i-1)/2 + j-i] = x;
+    }
+    int get(int i,int j) {
+        if(i<=j) return A[n*(i-1) - (i-2)*(i-1)/2 + j-i];
+        else return 0;
+    }
+    void display() {
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=n;j++) {
+                if(i<=j) cout<<get(i,j)<<" ";
+                else cout<<"0 ";
+            }
+            cout<<endl;
+        }
+    }
+};
