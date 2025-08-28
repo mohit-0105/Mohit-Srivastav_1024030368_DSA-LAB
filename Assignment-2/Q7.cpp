828cd48955ce7c2111c57064f469ae7c926aae53
Mohit Srivastav <mohit.srivastav2006@gmail.com>
2025-08-28 23:49:38 +0530

    Update Q7.cpp

--- Assignment-2/Q7.cpp
+++ Assignment-2/Q7.cpp
@@ -1,45 +1,40 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
 using namespace std;
 
-long long merge(vector<int> &arr, int left, int mid, int right) {
-    int n1 = mid - left + 1, n2 = right - mid;
-    vector<int> L(arr.begin() + left, arr.begin() + mid + 1);
-    vector<int> R(arr.begin() + mid + 1, arr.begin() + right + 1);
+long long mergeAndCount(int arr[], int l, int m, int r) {
+    int n1 = m-l+1, n2 = r-m;
+    int L[n1], R[n2];
+    for(int i=0;i<n1;i++) L[i]=arr[l+i];
+    for(int i=0;i<n2;i++) R[i]=arr[m+1+i];
 
-    long long inv_count = 0;
-    int i = 0, j = 0, k = left;
+    int i=0,j=0,k=l;
+    long long inv=0;
 
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j])
-            arr[k++] = L[i++];
+    while(i<n1 && j<n2) {
+        if(L[i] <= R[j]) arr[k++] = L[i++];
         else {
             arr[k++] = R[j++];
-            inv_count += (n1 - i);
+            inv += (n1 - i); 
         }
     }
-
-    while (i < n1) arr[k++] = L[i++];
-    while (j < n2) arr[k++] = R[j++];
-
-    return inv_count;
+    while(i<n1) arr[k++] = L[i++];
+    while(j<n2) arr[k++] = R[j++];
+    return inv;
 }
 
-long long mergeSort(vector<int> &arr, int left, int right) {
-    long long inv_count = 0;
-    if (left < right) {
-        int mid = (left + right) / 2;
-        inv_count += mergeSort(arr, left, mid);
-        inv_count += mergeSort(arr, mid + 1, right);
-        inv_count += merge(arr, left, mid, right);
+long long mergeSortAndCount(int arr[], int l, int r) {
+    long long inv = 0;
+    if(l<r) {
+        int m = (l+r)/2;
+        inv += mergeSortAndCount(arr,l,m);
+        inv += mergeSortAndCount(arr,m+1,r);
+        inv += mergeAndCount(arr,l,m,r);
     }
-    return inv_count;
+    return inv;
 }
 
 int main() {
-    vector<int> arr = {2, 4, 1, 3, 5};
-    long long inversions = mergeSort(arr, 0, arr.size() - 1);
-
-    cout << "Number of inversions: " << inversions << endl;
-    return 0;
+    int arr[] = {2, 4, 1, 3, 5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<"Number of inversions: "<<mergeSortAndCount(arr,0,n-1)<<endl;
 }
